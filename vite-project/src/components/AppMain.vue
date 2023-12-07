7a1a46454bc251192ba651640b145fca5323e11c
DavideCiammetti <davideciammetti97@gmail.com>
2023-12-07 18:38:39 +0100

    feat: struttura main

--- vite-project/src/components/AppMain.vue
+++ vite-project/src/components/AppMain.vue
@@ -5,10 +5,32 @@
 </script>
 
 <template>
-     <div>
-        main
-    </div>
+    <main id="main">
+        <!-- menu -->
+        <div class="menu-container">
+            menu
+        </div>
+         <!-- menu -->
+          <!-- info -->
+        <div class="info-container">
+            info
+        </div>
+         <!-- info -->
+    </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use '../style/partials/variables.scss' as *;
+
+main{
+    height: 300px;
+}
+.menu-container{
+    background-color: $bg-blue;
+    height: 100px;
+}
+.info-container{
+    background-color: rgb(93, 93, 93);
+    height: 200px;
+}
 </style>
