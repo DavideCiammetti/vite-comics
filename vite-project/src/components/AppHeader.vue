cfe09724e6e217d52038ac7e23aafa038b8b3816
DavideCiammetti <davideciammetti97@gmail.com>
2023-12-07 17:30:57 +0100

    feat: installo scss importo il mio file in header

--- vite-project/src/components/AppHeader.vue
+++ vite-project/src/components/AppHeader.vue
@@ -5,11 +5,17 @@
 </script>
 
 <template>
-     <div>
-        header
-    </div>
+     <header id="header">
+        <div class="nav-container">div1</div>
+        <div class="content-container">div2</div>
+     </header>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use '../style/partials/variables.scss' as*;
+
+.content-container{
+    background-color: $bg-black;
+}
 
 </style>
