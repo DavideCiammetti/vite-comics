e4a1b619ede5ab857980369b9a0a643256b8374c
DavideCiammetti <davideciammetti97@gmail.com>
2023-12-07 21:23:27 +0100

    fix: aggiunta componenet nav staccato da header

--- vite-project/src/components/AppHeader.vue
+++ vite-project/src/components/AppHeader.vue
@@ -1,58 +1,20 @@
 <script>
-    import AppDcLogo from './AppDcLogo.vue';
+ import AppDcLogo from './AppDcLogo.vue';
+    import AppNavHead from './AppNavHead.vue';
      export default{
         name :'header', 
         components:{
             AppDcLogo,
+            AppNavHead,
         }
     }
 </script>
 
 <template>
-
      <header id="header">
-        <div class="nav-container">
-            <AppDcLogo></AppDcLogo>
-            <nav class="navbar">
-                <ul class="list-container">
-                    <li class="list">charters</li>
-                    <li class="list">comics</li>
-                    <li class="list">movies</li>
-                    <li class="list">tv</li>
-                    <li class="list">games</li>
-                    <li class="list">collectibles</li>
-                    <li class="list">videos</li>
-                    <li class="list">fans</li>
-                    <li class="list">news</li>
-                    <li class="list">shop</li>
-                </ul>
-            </nav>
-        </div>
+        <AppNavHead></AppNavHead>
      </header>
 </template>
 
-<style scoped lang="scss">
-@use '../style/partials/variables.scss' as*;
-// navbar container
-.nav-container{
-    display: $d-flex;
-    justify-content: space-around;
-    // navbar
-    .navbar{
-        display: $d-flex;
-        align-items: center;
-    }
-        // ul
-        .list-container{
-            display: $d-flex;
-            list-style: none;
-        }
-            // li
-            .list{
-                text-transform: uppercase;
-                padding: .8125rem;
-            }
-}
-// navbar container
-
+<style scoped>
 </style>
---
+++ vite-project/src/components/AppNavHead.vue
@@ -0,0 +1,55 @@
+<script>
+    import AppDcLogo from './AppDcLogo.vue';
+     export default{
+        name :'nav', 
+        components:{
+            AppDcLogo,
+        }
+    }
+</script>
+
+<template>
+    <div class="nav-container">
+        <AppDcLogo></AppDcLogo>
+        <nav class="navbar">
+            <ul class="list-container">
+                <li class="list">charters</li>
+                <li class="list">comics</li>
+                <li class="list">movies</li>
+                <li class="list">tv</li>
+                <li class="list">games</li>
+                <li class="list">collectibles</li>
+                <li class="list">videos</li>
+                <li class="list">fans</li>
+                <li class="list">news</li>
+                <li class="list">shop</li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables.scss' as*;
+// navbar container
+.nav-container{
+    display: $d-flex;
+    justify-content: space-around;
+    // navbar
+    .navbar{
+        display: $d-flex;
+        align-items: center;
+    }
+        // ul
+        .list-container{
+            display: $d-flex;
+            list-style: none;
+        }
+            // li
+            .list{
+                text-transform: uppercase;
+                padding: .8125rem;
+            }
+}
+// navbar container
+
+</style>
